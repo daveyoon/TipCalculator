8eecfcc75ea297acaf16437e9de268c05c373e50
David Yoon <dave.yoon@gmail.com>
2014-01-20 17:19:18 -0800

    first functional version of the tip calculator

--- gen/com/codepath/tipcalculator/R.java
+++ gen/com/codepath/tipcalculator/R.java
@@ -26,10 +26,10 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080005;
         public static final int btn_10=0x7f080001;
-        public static final int btn_15=0x7f080004;
+        public static final int btn_15=0x7f080003;
         public static final int btn_20=0x7f080002;
-        public static final int editText1=0x7f080000;
-        public static final int tvTipTotal=0x7f080003;
+        public static final int etAmountTotal=0x7f080000;
+        public static final int tvTipTotal=0x7f080004;
     }
     public static final class layout {
         public static final int activity_tip_calculator=0x7f030000;
@@ -40,11 +40,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int btn_10_label=0x7f050003;
-        public static final int btn_15_label=0x7f050004;
-        public static final int btn_20_label=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int lbl_results=0x7f050006;
+        public static final int btn_10_label=0x7f050002;
+        public static final int btn_15_label=0x7f050003;
+        public static final int btn_20_label=0x7f050004;
+        public static final int lbl_results=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/codepath/tipcalculator/TipCalculatorActivity.java
+++ src/com/codepath/tipcalculator/TipCalculatorActivity.java
@@ -1,22 +1,75 @@
 package com.codepath.tipcalculator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class TipCalculatorActivity extends Activity {
+	double tipPercent = 0.0; 
+	double totalAmount;
 
+	EditText etTotalAmount;
+	TextView tvTipTotal;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);								
 		setContentView(R.layout.activity_tip_calculator);
+		setupListViewListener(); 		
 	}
 
+	protected void setupListViewListener() {
+		Button btn10 = (Button) findViewById(R.id.btn_10);
+		btn10.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		    	tipPercent = 0.1; 
+		    			    	
+		    	calculateTipTotal();		    	
+		    }
+		});
+		Button btn15 = (Button) findViewById(R.id.btn_15);
+		btn15.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        tipPercent = 0.15;		        		        
+		        calculateTipTotal();		        
+		    }
+		});
+		Button btn20 = (Button) findViewById(R.id.btn_20);
+		btn20.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		    	tipPercent = 0.20; 		    			    	
+		    	calculateTipTotal();		    	
+		    }
+		});		
+	}
+	
+	protected void calculateTipTotal() {
+		etTotalAmount = (EditText) findViewById(R.id.etAmountTotal);
+		tvTipTotal = (TextView) findViewById(R.id.tvTipTotal);
+		try {
+			totalAmount = Double.valueOf(etTotalAmount.getText().toString());
+		} catch (NumberFormatException e){
+			totalAmount = 0.0; // if the user hasn't entered a value in yet, assume that the total is 0 
+		}
+		
+		Double tipTotal = Math.round( 100.00 * tipPercent * totalAmount) / 100.00 ;	
+		String text = "Tip is: $" + String.format("%.2f", tipTotal);
+		tvTipTotal.setText(text);				
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tip_calculator, menu);
 		return true;
 	}
-
 }
